0d33e221b392d8fc52084f19e95b66ee6d9772f4
László Keszeg <keszeglaszlof@gmail.com>
2020-10-04 19:56:13 +0200

    2.coommit

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,10 +16,52 @@ namespace WindowsFormsApp1
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xcSheet;
+
         public Form1()
         {
             InitializeComponent();
 
+        }
+
+        private void CreateExcel ()
+        {
+
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xcSheet = xlWB.ActiveSheet;
+
+                createTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLIne: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void createTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód","Eladó","Oldal","Kerület","Lift","Szobák Száma","Alapterület","Ár","nm/ár"
+            };
         }
 
         private void Form1_Load(object sender, EventArgs e)
