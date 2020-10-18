30eea8416f8f9bb16ff0dd69b00cf77f672e801d
László Keszeg <keszeglaszlof@gmail.com>
2020-10-18 20:33:56 +0200

    masoidik

--- 6.anyag/6.anyag/Form1.cs
+++ 6.anyag/6.anyag/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.anyag.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,54 @@ namespace _6.anyag
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            ElsoFveny();
+        }
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
+
+
+        /* public void ElsoFveny()
+        {
+           // var mnbService = new MNBArfolyamServiceSoapBody()
+
+           var request = new GetExchangeRatesRequestBody();
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                enDate = "2020-06-30"
+
+            };
+            
+            var response = mnbService.GetExchangeRates(request);
+            
+            var result = response.GetExchangeRatesResult;
+
+        }
+        */
+        public void XML()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
